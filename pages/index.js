a2d4cd34a6e1e835a050442b4f3457490736fcc2
prashant-kumar96 <prashant.kumar560@gmail.com>
2022-11-09 21:11:27 +0530

    success

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="page2.js">Click here to go to page2</Link>
+      <Link href="/page2">Click here to go to page2</Link>
     </>
   );
 }
